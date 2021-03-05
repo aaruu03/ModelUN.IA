6da3dfb039840ed18b3096c4ab8ee66de0d4ae1c
aaruu03 <berryaarya@gmail.com>
2021-03-05 01:16:25 -0800

    created backend and can connect to front end now

---
+++ backend/app/config/auth.config.js
@@ -0,0 +1,3 @@
+module.exports = {
+    secret: "simunulation-secret-key"
+  };
---
+++ backend/app/config/db.config.js
@@ -0,0 +1,3 @@
+module.exports = {
+    mongoURI: "mongodb+srv://aaruu:50sporks@cluster0.q8i9l.mongodb.net/ModelUN?retryWrites=true&w=majority"
+  };
